17a08c59778545cfcddbe0d974ce5c308ded16aa
AOMuiz <abdulwasiumuiz@gmail.com>
2024-05-10 18:58:18 +0100

    feat: fix issue after dynamic url update for popular links

--- src/pages/[provider]/index.tsx
+++ src/pages/[provider]/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Slug from "../../components/Slug";
 
-const SlugPage = () => {
+const Index = () => {
   return <Slug />;
 };
 
-export default SlugPage;
+export default Index;
