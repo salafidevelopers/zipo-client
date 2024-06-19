c77714e97f30af97cc4830ed3d1e50f43038b93a
AOMuiz <abdulwasiumuiz@gmail.com>
2024-06-19 08:27:40 +0100

    chore

--- src/pages/stat/[slug].tsx
+++ src/pages/stat/[slug].tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import StatPage from "../../components/stats/Stat";
 import { Box, Text } from "@chakra-ui/react";
+import HeadComponent from "../../components/Head";
+import RawHeader from "../../components/RawHeader";
 
 function Stat() {
   return (
     <Box>
+      <HeadComponent title="Statistics" />
+      <RawHeader fixed={false} />
       <Text fontSize="3xl">Coming Soon...</Text>
       {/* <StatPage /> */}
     </Box>
