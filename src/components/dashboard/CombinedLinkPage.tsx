88366669f4a777c9b094aa8345a90586be8190d5
AOMuiz <abdulwasiumuiz@gmail.com>
2024-06-28 07:12:12 +0100

    cleanup

--- src/components/dashboard/CombinedLinkPage.tsx
+++ src/components/dashboard/CombinedLinkPage.tsx
@@ -1,9 +1,7 @@
 import {
   Box,
-  Button,
   Card,
   CardBody,
-  Flex,
   HStack,
   Heading,
   Text,
@@ -22,11 +20,18 @@ function CombinedLinkPage({ link }) {
     <VStack justify={"space-between"} minH="100vh">
       <RawHeader fixed={false} />
 
-      <VStack spacing={2} maxW={{ base: "250px", md: "500px" }} pb={14}>
+      <VStack
+        spacing={2}
+        maxW={{ base: "100%", md: "500px" }}
+        pb={14}
+        align={"center"}
+      >
         <Box mb={3}>
           <Image alt="zipo" src="Link_Blue.svg" width={100} height={100} />
         </Box>
-        <Heading fontWeight={400}>{content.title}</Heading>
+        <Heading fontWeight={400} textAlign={"center"}>
+          {content.title}
+        </Heading>
         <Text fontSize={"13px"}>{content.description}</Text>
         <VStack>
           <HStack spacing={4}>
@@ -56,24 +61,22 @@ function CombinedLinkPage({ link }) {
               }
             })}
           </HStack>
-          <VStack spacing={4} mt={4}>
+          <VStack spacing={4} mt={4} mx={4}>
             {content.links.map((link) => {
               const isOtherLink = link.title === "others";
               if (isOtherLink) {
                 return (
                   <Card
                     key={link.id}
-                    maxW="fit-content"
+                    w={"100%"}
                     boxShadow="lg"
                     borderRadius="md"
                     direction={"row"}
-                    bg={"zipo.500"}
                   >
                     <CardBody p={2}>
                       <div
                         style={{
                           display: "flex",
-                          justifyContent: "center",
                           alignItems: "center",
                           gap: "20px",
                         }}
@@ -86,7 +89,12 @@ function CombinedLinkPage({ link }) {
                             src={socialLinks[socialLinks.length - 1].src}
                           />
                         </a>
-                        <Text fontSize="sm" color="white" fontWeight="bold">
+                        <Text
+                          fontSize="sm"
+                          color="black"
+                          fontWeight="bold"
+                          noOfLines={1}
+                        >
                           <a href={link.url} target="_blank">
                             {link.url}
                           </a>
